471558c0127de881df344946cd253e10148a43ba
jyotiraditya06 <adityapatil065@gmail.com>
2023-05-05 14:43:17 +0530

    Changes

--- BubbleSort/bubblesortAlgo.java
+++ BubbleSort/bubblesortAlgo.java
@@ -5,8 +5,10 @@ import java.util.Arrays;
 public class bubblesortAlgo {
 
     static void bubble(int[] arr) {
+        boolean swapped;
         // run the steps n-1 times
         for (int i = 0; i < arr.length; i++) {
+            swapped = false;
             // for each step, max iteam will come at the last respective index
             for (int j = 1; j < arr.length - i; j++) {
                 // swap if the iteam is smaller than the previous item
@@ -15,7 +17,12 @@ public class bubblesortAlgo {
                     int temp = arr[j];
                     arr[j] = arr[j - 1];
                     arr[j - 1] = temp;
+                    swapped = true;
                 }
+            } // if you didnt swap for a particular value of i, it means the array is sorted
+              // hence stop the program
+            if (!swapped) {
+                break;
             }
         }
     }
