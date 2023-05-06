58167a8663395ee31be5ed040d7961702a20b43c
jyotiraditya06 <adityapatil065@gmail.com>
2023-05-06 21:38:05 +0530

    Updated

--- Bineary Search/SearchInsertPosition.java
+++ Bineary Search/SearchInsertPosition.java
@@ -4,17 +4,21 @@ public class SearchInsertPosition {
 
     class Solution {
         public int searchInsert(int[] nums, int target) {
-            int low = 0, high = A.length - 1;
-            while (low <= high) {
-                int mid = (low + high) / 2;
-                if (A[mid] == target)
+            int start = 0;
+            int end = nums.length - 1;
+
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+
+                if (target == nums[mid]) {
                     return mid;
-                else if (A[mid] > target)
-                    high = mid - 1;
-                else
-                    low = mid + 1;
+                } else if (target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
             }
-            return low;
+            return start;
         }
     }
 
