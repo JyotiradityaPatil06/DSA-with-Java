33fe4414e693ec14abe31c192db27308b23ff0c1
jyotiraditya06 <adityapatil065@gmail.com>
2023-05-09 23:32:32 +0530

    ..

---
+++ Bineary Search/FirstBadVersion.java
@@ -0,0 +1,27 @@
+// Leetcode 278 - First Bad Version
+
+public class FirstBadVersion {
+    public class Solution extends VersionControl {
+        public int firstBadVersion(int n) {
+            int start = 0;
+            int end = n;
+
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+
+                if (isBadVersion(mid) == true && isBadVersion(mid - 1) == false) {
+                    return mid;
+                } else if (isBadVersion(mid) == false) {
+                    start = mid + 1;
+                } else {
+                    end = mid;
+                }
+            }
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
