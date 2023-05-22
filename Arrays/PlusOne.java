90cc63669e63cbc878694c300940017ea7a74909
jyotiraditya06 <adityapatil065@gmail.com>
2023-05-22 23:25:15 +0530

    leetcode 66

---
+++ Arrays/PlusOne.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class PlusOne {
+
+    class Solution {
+        public int[] plusOne(int[] digits) {
+            if (digits == null) {
+                throw new IllegalArgumentException("Input array is null");
+            }
+
+            int len = digits.length;
+            if (len == 0) {
+                return new int[] { 1 };
+            }
+
+            for (int i = len - 1; i >= 0; i--) {
+                if (digits[i] != 9) {
+                    digits[i]++;
+                    return digits;
+                }
+                digits[i] = 0;
+            }
+
+            digits = new int[len + 1];
+            digits[0] = 1;
+            return digits;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
