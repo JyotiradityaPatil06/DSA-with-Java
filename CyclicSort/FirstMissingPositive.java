f41f80ddcf829164e44ea0f059af8bc85c9d79bb
jyotiraditya06 <adityapatil065@gmail.com>
2023-11-02 21:42:39 +0530

    Leetcode 41 (Hard)

---
+++ CyclicSort/FirstMissingPositive.java
@@ -0,0 +1,32 @@
+package CyclicSort;
+
+public class FirstMissingPositive {
+    lass Solution
+    {
+
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correctInd = nums[i] - 1;
+            if (nums[i] > 0 && nums[i] < nums.length && nums[i] != nums[correctInd]) {
+                swap(nums, i, correctInd);
+            } else {
+                i++;
+            }
+        }
+
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                return index + 1;
+            }
+        }
+
+        return nums.length + 1;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}}
