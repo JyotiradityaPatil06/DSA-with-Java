33130889580ac86f736b2e41ce638d238dd389dc
jyotiraditya06 <adityapatil065@gmail.com>
2023-11-03 21:27:09 +0530

    changes

--- CyclicSort/FirstMissingPositive.java
+++ CyclicSort/FirstMissingPositive.java
@@ -1,32 +1,33 @@
 package CyclicSort;
 
+//Leetcode 41
 public class FirstMissingPositive {
-    lass Solution
-    {
+    class Solution {
 
-    public int firstMissingPositive(int[] nums) {
-        int i = 0;
-        while (i < nums.length) {
-            int correctInd = nums[i] - 1;
-            if (nums[i] > 0 && nums[i] < nums.length && nums[i] != nums[correctInd]) {
-                swap(nums, i, correctInd);
-            } else {
-                i++;
+        public int firstMissingPositive(int[] nums) {
+            int i = 0;
+            while (i < nums.length) {
+                int correctInd = nums[i] - 1;
+                if (nums[i] > 0 && nums[i] < nums.length && nums[i] != nums[correctInd]) {
+                    swap(nums, i, correctInd);
+                } else {
+                    i++;
+                }
             }
+
+            for (int index = 0; index < nums.length; index++) {
+                if (nums[index] != index + 1) {
+                    return index + 1;
+                }
+            }
+
+            return nums.length + 1;
         }
 
-        for (int index = 0; index < nums.length; index++) {
-            if (nums[index] != index + 1) {
-                return index + 1;
-            }
+        static void swap(int[] arr, int first, int second) {
+            int temp = arr[first];
+            arr[first] = arr[second];
+            arr[second] = temp;
         }
-
-        return nums.length + 1;
     }
-
-    static void swap(int[] arr, int first, int second) {
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
-    }
-}}
+}
