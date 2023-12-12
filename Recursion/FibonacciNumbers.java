f3edecf8908cde338d81ae771b0e5d4f381badcc
jyotiraditya06 <adityapatil065@gmail.com>
2023-12-12 16:04:32 +0530

    Recursion (Fibonacci Numbers)

---
+++ Recursion/FibonacciNumbers.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+/**
+ * FibonacciNumbers
+ */
+public class FibonacciNumbers {
+
+    public static void main(String[] args) {
+        System.out.println(fibo(6));
+    }
+
+    static int fibo(int n) {
+
+        // base condition
+        if (n < 2) {
+            return n;
+        }
+
+        // recurance relartion
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
