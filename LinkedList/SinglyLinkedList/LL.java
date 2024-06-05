e378f7762f92bef6876b1fe5f48e90cf8721930c
jyotiraditya06 <adityapatil065@gmail.com>
2024-06-05 15:06:17 +0530

    Singly Linked List

---
+++ LinkedList/SinglyLinkedList/LL.java
@@ -0,0 +1,138 @@
+package LinkedList;
+
+import Recursion.nto1;
+
+public class LL {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LL() {
+        this.size = 0;
+    }
+
+    // HEAD INSERTION
+    public void insertFirst(int val) {
+        Node node = new Node(val); // A new 'Node' object is created with the given value 'val'
+        node.next = head; // 'next' reference of this new node is set to the current 'head'
+        head = node; // haed is updated to this new node
+
+        if (tail == null) {
+            tail = head;
+        }
+        size += 1;
+    }
+
+    // TAIL INSERTION
+    public void insertLast(int val) {
+
+        if (tail == null) {
+            insertFirst(val);
+            return;
+        }
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+    // NODE INSERTION AT GIVEN INDEX
+    public void insert(int val, int index) {
+        if (index == 0) {
+            insertFirst(val);
+            return;
+        }
+        if (index == size) {
+            insertLast(val);
+            return;
+        }
+
+        Node temp = head;
+        for (int i = 1; i < index; i++) {
+            temp = temp.next;
+        }
+
+        Node node = new Node(val, temp.next);
+        temp.next = node;
+        size++;
+
+    }
+
+    // DELETING HEAD
+    public int deleteFirst() {
+        int val = head.value;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return val;
+    }
+
+    // DELETING TAIL
+    public int deleteLast() {
+        if (size <= 1) {
+            deleteFirst();
+        }
+
+        Node secondLast = get(size - 2);
+        int val = tail.value;
+        tail = secondLast;
+        tail.next = null;
+        return val;
+    }
+
+    // DELETING A NODE
+    public int delete(int index) {
+        if (index == 0) {
+            return deleteFirst();
+        }
+
+        if (index == size - 1) {
+            return deleteLast();
+        }
+
+        Node prev = get(index - 1);
+        int val = prev.next.value;
+        prev.next = prev.next.next;
+
+        return val;
+    }
+
+    public Node get(int index) {
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    // PRINTING A NODE
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+
+    private class Node {
+        private int value;
+        private Node next; // next: This is a property of the Node class, which holds a reference to the
+                           // next node in the linked list.
+
+        // initializes a node with a value and sets the 'next' reference to null.
+        public Node(int value) {
+            this.value = value;
+        }
+
+        // Initializes a node with a value and sets the next reference to point to
+        // another node.
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
---
+++ LinkedList/main.java
@@ -0,0 +1,28 @@
+package LinkedList;
+
+public class main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(8);
+        list.insertFirst(6);
+        list.insertFirst(5);
+        list.insertFirst(17);
+        list.display();
+
+        list.insertLast(88);
+        list.display();
+
+        list.insert(100, 4);
+        list.display();
+
+        list.deleteFirst();
+        list.display();
+
+        list.deleteLast();
+        list.display();
+
+        list.delete(3);
+        list.display();
+    }
+}
